b832aa742f0db98600865bed7346ed6e7c93699f
Xanthorrhizol <xanthorrhizol@protonmail.com>
2021-05-08 02:16:09 +0900

    docker install

---
+++ components/virtual_machine/install.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+sudo apt remove -y docker docker-engine docker.io containerd runc
+sudo apt install -y ca-certificates curl gnupg lsb-release apt-transport-https
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io
