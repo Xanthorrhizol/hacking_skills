07aa138fd171adbdb1f07c353f3b1c78b435859e
Xanthorrhizol <xanthorrhizol@protonmail.com>
2021-06-03 05:01:27 +0900

    fix Empire

--- components/powershell_empire/setup.sh
+++ components/powershell_empire/setup.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 # install python2
-sudo apt install python2
-
-# install pip2
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-sudo python2 get-pip.py
-rm get-pip.py
+sudo apt install python2 swig m2crypto
 
 # install empire
 cd /opt
-sudo git clone https://github.com/PowerShellEmpire/Empire.git
+sudo git clone https://github.com/JuyunLee/Empire.git
 cd Empire
 sudo ./setup/install.sh
 
-# install pip packages
-sudo pip2 install setuptools
-sudo pip2 install flask cryptography iptools netifaces pydispatch
+# start
 sudo ./empire
 
 
