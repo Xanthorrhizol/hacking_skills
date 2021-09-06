f70db2fa7333d1510fb50dc84092a609a543dd33
Xanthorrhizol <45154504+Xanthorrhizol@users.noreply.github.com>
2021-09-06 23:38:54 +0900

    Update setup.sh

--- components/powershell_empire/setup.sh
+++ components/powershell_empire/setup.sh
@@ -5,7 +5,7 @@ sudo apt install python2 swig m2crypto
 
 # install empire
 cd /opt
-sudo git clone https://github.com/JuyunLee/Empire.git
+sudo git clone https://github.com/Xanthorrhizol/Empire.git
 cd Empire
 sudo ./setup/install.sh
 
